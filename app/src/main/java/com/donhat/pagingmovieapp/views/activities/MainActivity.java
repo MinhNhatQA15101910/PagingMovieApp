c19ce43c7adce22644cd6b88825c5586c5fd9600
Do Nhat <dmnhat.pt@gmail.com>
2024-04-09 13:58:19 +0700

    Creating Layouts

--- app/src/main/java/com/donhat/pagingmovieapp/views/activities/MainActivity.java
+++ app/src/main/java/com/donhat/pagingmovieapp/views/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.donhat.pagingmovieapp;
+package com.donhat.pagingmovieapp.views.activities;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.donhat.pagingmovieapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
