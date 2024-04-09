8ce8748b3cbeb2a4de54a722bc032302eedce629
Do Nhat <dmnhat.pt@gmail.com>
2024-04-09 14:23:47 +0700

    Creating Models

---
+++ app/src/main/java/com/donhat/pagingmovieapp/models/entities/Movie.java
@@ -0,0 +1,62 @@
+package com.donhat.pagingmovieapp.models.entities;
+
+import androidx.annotation.Nullable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Movie {
+    @SerializedName("id")
+    @Expose
+    private int id;
+
+    @SerializedName("title")
+    @Expose
+    private String title;
+
+    @SerializedName("poster_path")
+    @Expose
+    private String posterPath;
+
+    @SerializedName("vote_average")
+    @Expose
+    private double voteAverage;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (obj == null) return false;
+        return obj == this;
+    }
+}
---
+++ app/src/main/java/com/donhat/pagingmovieapp/models/entities/MovieResponse.java
@@ -0,0 +1,56 @@
+package com.donhat.pagingmovieapp.models.entities;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MovieResponse {
+    @SerializedName("page")
+    @Expose
+    private int page;
+
+    @SerializedName("results")
+    @Expose
+    private List<Movie> movies = null;
+
+    @SerializedName("total_results")
+    @Expose
+    private int totalMovies;
+
+    @SerializedName("total_pages")
+    @Expose
+    private int totalPages;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public int getTotalMovies() {
+        return totalMovies;
+    }
+
+    public void setTotalMovies(int totalMovies) {
+        this.totalMovies = totalMovies;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+}
